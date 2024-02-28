6adf7d0900c9f52f6a00c182eea6fbdfdab86891
Niharika811 <113079754+Niharika811@users.noreply.github.com>
2024-02-28 21:58:43 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css'
+
+function Main() {
+  return (
+    <div className='main'>
+        <img src='logo.png' className='logo'/>
+      <h5 className='heading'>C.R.I. PUMPS WINS THE NATIONAL ENERGY CONSERVATION AWARD 2018 for the 4th time.</h5>
+      <ul className='para'>
+        <li>C.R.I.'s energy efficient products are well recognized by various Government Institutions, as trustworthy products for various projects across the globe to save energy.
+        </li>
+        <li>C.R.I. is the highest contributor in the country for the projects of EESL (Energy Efficiency Services Limited) to replace the old inefficient pumps with 5 Star rated energy efficient smart pumps with IoT enabled control panel.
+        </li>
+      </ul>
+      <div className='img-container'>
+        <img src='1.png' className='img1'/>
+        <img src='2.png' className='img2'/>
+      </div>
+      <p className='para2'>Government of India has awarded the "<span className='bold-text'>National Energy Conservation Award 2018</span>". Mr. G. Selvaraj, Joint Managing Director of C.R.I. Group received the award from Smt. Sumitra Mahajan, Speaker of Lok Sabha & Shri. Raj Kumar Singh, Honorable Minister of State.</p>
+      <p className='para3'>INSTALLED OVER 10 LAKHS STAR RATED PUMPSETS ACROSS THE COUNTRY RESULTING IN A CUMULATIVE SAVING OF MORE THAN 9,000 MILLION UNITS OF POWER FOR THE NATION.</p>
+      <img src='3.png' className='img3'/>
+      <p>Valves - Pumps - Pipes - IoT Drives & Controllers - Wires & Cables - Solar Systems - Motors </p>
+      <div className='line'></div>
+      <h5>C.R.I. FLUID SYSTEMS PRODUCTS CATER TO DIVERSE SEGMENTS</h5>
+      <p>
+        CHEMICALS & PROCESS <span className="pipe">|</span> POWER WATER <span className="pipe">|</span> WASTE & WATER WATER <span className="pipe">|</span> OILS & GAS <span className="pipe">|</span> PHARMA <span className="pipe">|</span> SUGARS & DISTILLERIES <span className="pipe">|</span> PAPER & PULP <span className="pipe">|</span> MARINE & DEFENCE <span className="pipe">|</span> METAL & MINING <span className="pipe">|</span> FOOD & BEVERAGE <span className="pipe">|</span> PETROCHEMICAL & REFINERIES <span className="pipe">|</span> SOLAR <span className="pipe">|</span> BUILDING <span className="pipe">|</span> HVAC <span className="pipe">|</span> FIRE FIGHTING <span className="pipe">|</span> AGRICULTURE & RESIDENTIAL
+        </p>
+
+    </div>
+  );
+}
+
+export default Main;
